28d79fd44e97a57948214254778ef340930df730
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-08-09 16:03:56 +0100

    added sidebar component and flowbit to tailwind css and installed some pakages and some shit

---
+++ frontend/src/app/(admine)/home/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '@/components/utils/Sidebar'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default page
