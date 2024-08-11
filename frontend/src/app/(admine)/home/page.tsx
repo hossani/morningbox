73ef9c8edea24d497daae03c8a35621d4f744717
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-08-11 02:52:38 +0100

    "Added NavComponent to admin home page and refactored Sidebar component with updated styles and removed SVG icons"

--- frontend/src/app/(admine)/home/page.tsx
+++ frontend/src/app/(admine)/home/page.tsx
@@ -1,9 +1,11 @@
+import  NavComponent  from '@/components/Nav'
 import Sidebar from '@/components/utils/Sidebar'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
+            <NavComponent/>
             <Sidebar />
         </div>
     )
--- frontend/src/components/utils/Sidebar.tsx
+++ frontend/src/components/utils/Sidebar.tsx
@@ -4,53 +4,23 @@ import React from 'react'
 const Sidebar = () => {
     return (
         <div>
-            <aside
-                id="logo-sidebar"
-                className="top-0 left-0 z-40 w-64 h-screen  sm:translate-x-0 sidebar-class"
-                aria-label="Sidebar"
-            >
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 background-aside">
+            <aside id="logo-sidebar" className="top-0 left-0 z-40 w-64 sm:translate-x-0 bg-[#1F2937] h-screen" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto dark:bg-gray-800 bg-[#212529] h-auto;">
                     <a href="https://flowbite.com/" className="flex items-center ps-2.5 mb-5">
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white color-text-aside">
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-[white]">
                             Dashboard
                         </span>
                     </a>
                     <ul className="space-y-2 font-medium">
-                        <link href="/afficher" />
                         <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg
-                                className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 22 21"
-                            >
-                                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                            </svg>
-                            <a className="ms-3 color-text-aside group-hover:text-black ">
-                                Afficher
-                            </a>
-                        </div>
-                        <link href="/ajouter" />
-                        <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg
-                                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 18 18"
-                            >
-                                <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                            </svg>
-                            <a className="flex-1 ms-3 whitespace-nowrap color-text-aside  group-hover:text-black">
-                                Ajouter
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2.2rem" height="2.2rem" viewBox="0 0 24 24"><path fill="white" d="M12 4c4.411 0 8 3.589 8 8s-3.589 8-8 8s-8-3.589-8-8s3.589-8 8-8m0-2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2m5 9h-4V7h-2v4H7v2h4v4h2v-4h4z"/></svg>
+                            <a className="ms-3 text-[white] group-hover:text-black ">
+                                Add Breackfast
                             </a>
                         </div>
                     </ul>
                 </div>
             </aside>
-
         </div>
     )
 }
